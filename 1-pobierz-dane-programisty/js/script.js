c0784856934eacfd91ac64847b84666a568b0a88
macmaz <maciej.mazurek87@gmail.com>
2017-08-26 19:43:26 +0200

    ajax json jquery load

--- 1-pobierz-dane-programisty/js/script.js
+++ 1-pobierz-dane-programisty/js/script.js
@@ -1,18 +1,16 @@
-
-
 $("#btn1").click(function(){
     
-var dane = [{
-   "imie": "Piotr",
-   "zawod": "Programista",
-   "nazwisko": "Lewinski",
-   "firma": "Akademia108"
-}]
-
-$.getJSON("http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108", function(wynik){
+$.ajax({ 
+    type: 'GET', 
+    url: 'http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108', 
+    data: { get_param: 'value' }, 
+    dataType: 'json',
+    success: function(wynik){
+        
 $.each(wynik, function(index, wartosc){
     
 $("div").append(index + ": " + wartosc + "\n");
         });
-    });
+}
+});
 });
